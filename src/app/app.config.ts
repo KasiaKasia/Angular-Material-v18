d371f270b9feae02f7928b330173c67fe6336bc7
--add <bulicka.alexandra@gmail.com>
2024-09-18 21:43:32 +0200

    [ feat / refactor ] - adding info. on page 404 + refactor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,7 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { loadingInterceptor } from './common/Interceptors/loading.interceptor';
 
 export const appConfig: ApplicationConfig = {
